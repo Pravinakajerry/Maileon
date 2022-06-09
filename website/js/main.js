66b013dafc1284145be2b1523fb7dfea458d17d4
Pravin M Singh <Jerryatbusiness@gmail.com>
2022-06-09 23:32:25 +0530

    Added About Us & Contact page
    1. Made minor css changes
    2. Linked to notion
    3. Added Contact Us page
    4. Added About Us page
    5. Modified Coming soon signal
    
    More coming soon

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,7 @@
-// Popup Settings
+console.log("This project is open source :) We belive in good for nothing");
+console.log("Visit github to contribute to web: https://github.com/Pravinakajerry/Maileon")
+
+/* Popup Settings
 $('#get-started').on('click',function() {
   $('.popup-wp').css({
     'display':'flex'+' ',
@@ -16,6 +19,12 @@ $('.close-popup').on('click',function() {
     'display':'none'+' ',
   });
 });
+
+$(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    $('.popup-wp').hide();
+  }
+}); */
 
 // Responsive Menu Settings 
 $('#menu').click(function() {
